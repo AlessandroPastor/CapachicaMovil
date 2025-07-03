7f08b0e2d51d74546da963e4685213d18e550972
Alessandro Pastor <alexkip159alex@gmail.com>
2025-07-03 10:36:19 -0500

    s

--- app/src/main/java/com/example/turismomovile/data/remote/dto/ventas/SaleDTO.kt
+++ app/src/main/java/com/example/turismomovile/data/remote/dto/ventas/SaleDTO.kt
@@ -10,5 +10,20 @@ data class SaleCreateDTO(
 
 @Serializable
 data class SaleResponse(
-    val message: String? = null
+    val message: String? = null,
+    val ventas: List<VentasResponse>? = null
+)
+
+@Serializable
+data class VentasResponse(
+    val id: String? = null,
+    val emprendedor_id: String? = null,
+    val payment_id: String? = null,
+    val reserva_id: String? = null,
+    val code: String? = null,
+    val BI: String? = null,
+    val IGV: String? = null,
+    val total: String? = null,
+    val updated_at: String? = null,
+    val created_at: String? = null,
 )
